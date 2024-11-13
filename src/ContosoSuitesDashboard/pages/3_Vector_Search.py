15b7ff741cf1f2e648418279d131262fdfd5ac81
ArneDecker3v08mk <110330852+ArneDecker3v08mk@users.noreply.github.com>
2024-11-13 12:26:24 +0000

    Try something 3

--- src/ContosoSuitesDashboard/pages/3_Vector_Search.py
+++ src/ContosoSuitesDashboard/pages/3_Vector_Search.py
@@ -56,7 +56,7 @@ def main():
                 # Display the results.
                 st.write("## Results")
                 # Exercise 3 Task 3 TODO #6: Display the results as a table.
-                st.table(vector_search_results)
+                st.table(query + "\n" + query_vector + "n" + vector_search_results)
                 
             else:
                 st.warning("Please enter a query.")
